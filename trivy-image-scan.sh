7c8c8232c07dcb2d0e3a644a12d77e6b1159b1c7
NHAILA Achraf <achraf.nhaila@gmail.com>
2023-06-29 17:03:37 +0200

    Update trivy-image-scan.sh

--- trivy-image-scan.sh
+++ trivy-image-scan.sh
@@ -6,7 +6,7 @@ echo $dockerImageName
 
 #docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
-docker run --rm -v $WORKSPACE:/root/.cache/  -e TRIVY_GITHUB_TOKEN='token_github' aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light openjdk:8-jdk-alpine
+docker run --rm -v $WORKSPACE:/root/.cache/  -e TRIVY_GITHUB_TOKEN='token_github' aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
     # Trivy scan result processing
     exit_code=$?
